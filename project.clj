193929807f765f1c1d2a4f5b1578e47e425eda89
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-02-25 21:57:10 +0100

    project initialized using 'lein new'

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject systems-toolbox "0.1.0-SNAPSHOT"
+  :description "Toolbox for building Systems in Clojure"
+  :url "https://github.com/matthiasn/systems-toolbox"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ test/systems_toolbox/core_test.clj
@@ -0,0 +1,7 @@
+(ns systems-toolbox.core-test
+  (:require [clojure.test :refer :all]
+            [systems-toolbox.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
