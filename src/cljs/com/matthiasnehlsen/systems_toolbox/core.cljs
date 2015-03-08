460f5d86a6659191cb1a1ccb856bec81c3228a26
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-08 18:46:16 +0100

    additional way of creating components

--- src/cljs/com/matthiasnehlsen/systems_toolbox/core.cljs
+++ src/cljs/com/matthiasnehlsen/systems_toolbox/core.cljs
@@ -1,6 +1,7 @@
 (ns com.matthiasnehlsen.systems-toolbox.core
   (:require-macros [cljs.core.async.macros :refer [go-loop]])
-  (:require [cljs.core.async :refer [<! >! chan put!]]))
+  (:require [cljs.core.match :refer-macros [match]]
+            [cljs.core.async :refer [<! >! chan put! buffer sliding-buffer dropping-buffer]]))
 
 (defn component-with-channels
   "Creates a component with attached in-chan and out-chan.
@@ -19,3 +20,34 @@
              (msg-fn (<! in-chan))
              (recur))
     {:in-chan in-chan :out-chan out-chan}))
+
+(defn make-chan-w-buf
+  [config]
+  (match config
+         [:sliding n]  (chan (sliding-buffer n))
+         [:dropping n] (chan (dropping-buffer n))
+         [:buffer n]   (chan (buffer n))
+         :else (prn "invalid: " config)))
+
+(defn make-chans-w-buf
+  [channels]
+  (into {} (map (fn [[k v]] [k (make-chan-w-buf v)]) channels)))
+
+(defn component-with-channels-2
+  "Creates a component with attached in-chan and out-chan.
+   It takes a function fn and both an in- and an out-buffer.
+   The function fn is called with the internal put-fn that puts
+   messages onto the out-chan. Fn is expected to return another
+   function that can then be called for each message on the
+   in-chan. Component state can thus be kept inside the initial
+   call to fn."
+  [fn channel-config out-chan-selector]
+  (let [in-chan (make-chan-w-buf (:in-chan channel-config))
+        out-chans (make-chans-w-buf (:out-chans channel-config))
+        put-fn #(put! ((out-chan-selector %) out-chans) %)
+        msg-fn (fn put-fn)]
+    (go-loop []
+             (msg-fn (<! in-chan))
+             (recur))
+    {:in-chan in-chan :out-chans out-chans}))
+
