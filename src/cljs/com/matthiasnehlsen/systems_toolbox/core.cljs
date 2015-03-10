d657256af62bb73e8e2a67e23ad1db57cf249510
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-10 23:43:38 +0100

    generalized component model

--- src/cljs/com/matthiasnehlsen/systems_toolbox/core.cljs
+++ src/cljs/com/matthiasnehlsen/systems_toolbox/core.cljs
@@ -54,3 +54,47 @@
               (when-let [t (:in-timeout channel-config)] (<! (timeout t)))
               (recur))
      {:in-chan in-chan :out-chan out-chan})))
+
+(def component-defaults {:in-chan [:buffer 1]
+                         :out-chan [:buffer 1]
+                         :sliding-in-chan [:sliding 1]
+                         :sliding-out-chan [:sliding 1]
+                         :sliding-in-timeout 20})
+
+(defn make-component
+  "Creates a component with attached in-chan, out-chan, sliding-in-chan
+  and sliding-out-chan.
+  It takes the initial state atom, the handler function for messages on
+  in-chan, and the sliding-handler function, which handles messages on
+  sliding-in-chan.
+  By default, in-chan and out-chan have standard buffers of size one,
+  whereas sliding-in-chan and sliding-out-chan have sliding buffers of
+  size one. The buffer sizes can be configured.
+  The sliding-channels are meant for events where only ever the latest
+  version is of interest, such as mouse moves or published state
+  snapshots in the case of UI components rendering state snapshots from
+  other components."
+  ([state handler sliding-handler]
+   (make-component state handler sliding-handler component-defaults))
+  ([state handler sliding-handler cfg]
+   (let [out-chan (make-chan-w-buf (:out-chan cfg))
+         sliding-out-chan (make-chan-w-buf (:sliding-out-chan cfg))
+         put-fn #(put! out-chan %)]
+     (add-watch state :watcher (fn [_ _ _ new-state] (>! sliding-out-chan [:state new-state])))
+     (merge
+       {:out-chan out-chan
+        :sliding-out-chan sliding-out-chan}
+       )
+     (when handler
+       (let [in-chan (make-chan-w-buf (:in-chan cfg))]
+         (go-loop []
+                  (handler state put-fn (<! in-chan))
+                  (recur))
+         {:in-chan in-chan}))
+     (when sliding-handler
+       (let [sliding-in-chan (make-chan-w-buf (:sliding-in-chan cfg))]
+         (go-loop []
+                  (sliding-handler state put-fn (<! sliding-in-chan))
+                  (recur))
+         {:sliding-in-chan sliding-in-chan})))))
+
