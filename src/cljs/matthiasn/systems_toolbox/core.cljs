c2fead0e552efcc6006099ceb8d44dda6b679724
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-12 15:26:26 +0100

    refactoring: DRYer, leaner general component

--- src/cljs/matthiasn/systems_toolbox/core.cljs
+++ src/cljs/matthiasn/systems_toolbox/core.cljs
@@ -7,31 +7,39 @@
   "Create a channel with a buffer of the specified size and type."
   [config]
   (match config
-         [:sliding n]  (chan (sliding-buffer n))
-         [:dropping n] (chan (dropping-buffer n))
-         [:buffer n]   (chan (buffer n))
-         :else (prn "invalid: " config)))
+         [:sliding  n]  (chan (sliding-buffer n))
+         [:dropping n]  (chan (dropping-buffer n))
+         [:buffer   n]  (chan (buffer n))
+         :else          (prn "invalid: " config)))
 
-(def component-defaults {:in-chan [:buffer 1]
-                         :out-chan [:buffer 1]
-                         :sliding-in-chan [:sliding 1]
-                         :sliding-out-chan [:sliding 1]
-                         :sliding-in-timeout 5
-                         :atom true})
+(def component-defaults
+  {:in-chan  [:buffer 1]  :sliding-in-chan  [:sliding 1]  :sliding-in-timeout 5
+   :out-chan [:buffer 1]  :sliding-out-chan [:sliding 1]  :atom true})
+
+(defn msg-handler-loop
+  "Constructs a map with a channel for the provided channel keyword, with the buffer
+  configured according to cfg for the channel keyword. Then starts loop for taking messages
+  off the returned channel and calling the provided handler-fn with the msg.
+  Does not process return values from the processing step; instead, put-fn needs to be
+  called to produce output."
+  [state handler-fn put-fn cfg chan-key]
+  (when handler-fn
+    (let [chan (make-chan-w-buf (chan-key cfg))]
+      (go-loop []
+               (handler-fn state put-fn (<! chan))
+               (recur))
+      {chan-key chan})))
 
 (defn make-component
-  "Creates a component with attached in-chan, out-chan, sliding-in-chan
-  and sliding-out-chan.
-  It takes the initial state atom, the handler function for messages on
-  in-chan, and the sliding-handler function, which handles messages on
-  sliding-in-chan.
-  By default, in-chan and out-chan have standard buffers of size one,
-  whereas sliding-in-chan and sliding-out-chan have sliding buffers of
-  size one. The buffer sizes can be configured.
-  The sliding-channels are meant for events where only ever the latest
-  version is of interest, such as mouse moves or published state
-  snapshots in the case of UI components rendering state snapshots from
-  other components."
+  "Creates a component with attached in-chan, out-chan, sliding-in-chan and sliding-out-chan.
+  It takes the initial state atom, the handler function for messages on in-chan, and the
+  sliding-handler function, which handles messages on sliding-in-chan.
+  By default, in-chan and out-chan have standard buffers of size one, whereas sliding-in-chan
+  and sliding-out-chan have sliding buffers of size one.
+  The buffer sizes can be configured.
+  The sliding-channels are meant for events where only ever the latest version is of interest,
+  such as mouse moves or published state snapshots in the case of UI components rendering
+  state snapshots from other components."
   ([mk-state handler sliding-handler]
    (make-component mk-state handler sliding-handler component-defaults))
   ([mk-state handler sliding-handler opts]
@@ -46,27 +54,5 @@
      (merge
        {:out-mult out-mult
         :state-pub (pub sliding-out-chan first)}
-       (when handler
-         (let [in-chan (make-chan-w-buf (:in-chan cfg))]
-           (go-loop []
-                    (let [msg (<! in-chan)]
-                      (handler state put-fn msg)
-                      (recur)))
-           {:in-chan in-chan}))
-       (when sliding-handler
-         (let [sliding-in-chan (make-chan-w-buf (:sliding-in-chan cfg))]
-           (go-loop []
-                    (sliding-handler state put-fn (<! sliding-in-chan))
-                    (recur))
-           {:sliding-in-chan sliding-in-chan}))))))
-
-(defn state-snapshot-subscribe
-  [pub-comp key sub-comps]
-  (doseq [c sub-comps]
-    (sub (:state-pub pub-comp) key (:sliding-in-chan c))))
-
-(defn pipe-multiple
-  [comps]
-  (doseq [[from to] comps]
-    (pipe (:out-chan from) (:in-chan to))))
-
+       (msg-handler-loop state handler put-fn cfg :in-chan)
+       (msg-handler-loop state sliding-handler put-fn cfg :sliding-in-chan)))))
