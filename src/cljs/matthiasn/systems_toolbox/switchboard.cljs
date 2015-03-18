c08a45fc64aaa1eafc399ad09a136bec5cf8f67f
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-18 18:49:36 +0100

    switchboard logging through log component

--- src/cljs/matthiasn/systems_toolbox/switchboard.cljs
+++ src/cljs/matthiasn/systems_toolbox/switchboard.cljs
@@ -7,54 +7,59 @@
             [matthiasn.systems-toolbox.sente :as ws]))
 
 (defn make-comp
-  [app cfg]
+  [app put-fn cfg]
   (let [{:keys [cmp-id mk-state-fn handler-fn state-pub-handler-fn opts]} cfg
         cmp (comp/make-component mk-state-fn handler-fn state-pub-handler-fn opts)]
-    (prn "Creating component:" cmp-id)
+    (put-fn [:log/switchboard-init cmp-id])
     (swap! app assoc-in [:components cmp-id] cmp)))
 
 (defn subscribe-component
   "Subscribe component to a specified publisher."
-  [app params]
+  [app put-fn params]
   (let [pub-comp ((:pub-comp params) (:components @app))
         sub-comp ((:sub-comp params) (:components @app))]
     (sub (:state-pub pub-comp) :app-state (:sliding-in-chan sub-comp))
-    (prn "Sub:" (:pub-comp params) "->" (:sub-comp params))
+    (put-fn [:log/switchboard-sub (str (:pub-comp params) "->" (:sub-comp params))])
     (swap! app update-in [:subs] conj params)))
 
 (defn tap-comp
-  "Tap component channel to a specified mult."
-  [app params]
+  "Tap component in channel to a specified mult."
+  [app put-fn params]
   (let [mult-comp ((:mult-comp params) (:components @app))
         tap-comp  ((:tap-comp params)  (:components @app))]
     (tap (:out-mult mult-comp) (:in-chan tap-comp))
-    (prn "Tap:" (:mult-comp params) "->" (:tap-comp params))
+    (put-fn [:log/switchboard-tap (str (:mult-comp params) "->" (:tap-comp params))])
     (swap! app update-in [:taps] conj params)))
 
 (defn make-ws-comp
   "Initializes Sente / WS component and makes is accessible under [:components :ws]
   inside the switchboard state atom."
-  [app]
+  [app put-fn]
   (let [ws (ws/component)]
     (swap! app assoc-in [:components :ws] ws)
-    (prn "Creating component:" :ws)
+    (put-fn [:log/switchboard-init :ws])
     ws))
 
 (defn make-reagent-comp
   "Creates a Reagent component."
-  [app params]
+  [app put-fn params]
   (let [{:keys [cmp-id view-fn dom-id init-state]} params
         cmp (r/component view-fn dom-id init-state)]
-    (prn "Creating Reagent component: " cmp-id)
+    (put-fn [:log/switchboard-init-reagent cmp-id])
     (swap! app assoc-in [:components cmp-id] cmp)))
 
 (defn make-log-comp
   "Creates a log component."
-  [app]
+  [app put-fn]
   (let [log-comp (l/component)]
     (swap! app assoc-in [:components :log] log-comp)
-    (prn "Creating component:" :log)
+    (put-fn [:log/switchboard-init :log])
     log-comp))
+
+(defn self-register
+  ""
+  [app put-fn self]
+  (swap! app assoc-in [:components :switchboard] self))
 
 (defn make-state
   "Return clean initial component state atom."
@@ -66,14 +71,15 @@
 
 (defn in-handler
   "Handle incoming messages: process / add to application state."
-  [app _ msg]
+  [app put-fn msg]
   (match msg
-         [:cmd/make-comp      cmp] (make-comp app cmp)
-         [:cmd/make-ws-comp      ] (make-ws-comp app)
-         [:cmd/make-log-comp     ] (make-log-comp app)
-         [:cmd/make-r-comp params] (make-reagent-comp app params)
-         [:cmd/sub-comp    params] (subscribe-component app params)
-         [:cmd/tap-comp    params] (tap-comp app params)
+         [:cmd/self-register self] (self-register app put-fn self)
+         [:cmd/make-comp      cmp] (make-comp app put-fn cmp)
+         [:cmd/make-ws-comp      ] (make-ws-comp app put-fn)
+         [:cmd/make-log-comp     ] (make-log-comp app put-fn)
+         [:cmd/make-r-comp params] (make-reagent-comp app put-fn params)
+         [:cmd/sub-comp    params] (subscribe-component app put-fn params)
+         [:cmd/tap-comp    params] (tap-comp app put-fn params)
          :else (prn "unknown msg in switchboard-in-loop" msg)))
 
 (defn component
@@ -81,4 +87,9 @@
   a communicating system."
   []
   (prn "Switchboard starting.")
-  (comp/make-component make-state in-handler nil))
+  (let [switchboard (comp/make-component make-state in-handler nil)
+        sw-in-chan (:in-chan switchboard)]
+    (put! sw-in-chan [:cmd/self-register switchboard])
+    (put! sw-in-chan [:cmd/make-log-comp])
+    (put! sw-in-chan [:cmd/tap-comp {:mult-comp :switchboard :tap-comp :log}])
+    switchboard))
