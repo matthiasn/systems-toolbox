dd61ab8bf7be863d06413b7f40e15881a70f59db
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-03-18 21:48:31 +0100

    WIP: Clojure implementation

---
+++ src/clj/matthiasn/systems_toolbox/sente.clj
@@ -0,0 +1,32 @@
+(ns matthiasn.systems-toolbox.sente
+  (:require [cljs.core.match :refer-macros [match]]
+            [matthiasn.systems-toolbox.component :as comp]
+            [taoensso.sente :as sente :refer (cb-success?)]))
+
+(defn make-handler
+  "Create handler function for messages from WebSocket connection. Calls put-fn with received
+   messages."
+  [put-fn]
+  (fn [{:keys [event]}]
+    (match event
+           [:chsk/state {:first-open? true}] (put-fn [:first-open true])
+           [:chsk/recv payload]              (put-fn payload)
+           [:chsk/handshake _]               ()
+           :else (print "Unmatched event: %s" event))))
+
+(defn make-state
+  "Return clean initial component state atom."
+  [put-fn]
+  (let [ws (sente/make-channel-socket! "/chsk" {:type :auto})]
+    (sente/start-chsk-router! (:ch-recv ws) (make-handler put-fn))
+    ws))
+
+(defn in-handler
+  "Handle incoming messages: process / add to application state."
+  [ws _ [cmd-type payload]]
+  (let [state (:state ws)]
+    ((:send-fn ws) [cmd-type (assoc payload :uid (:uid @state))])))
+
+(defn component
+  []
+  (comp/make-component make-state in-handler nil {:atom false}))
---
+++ src/clj/matthiasn/systems_toolbox/switchboard.clj
@@ -0,0 +1,95 @@
+(ns matthiasn.systems-toolbox.switchboard
+  (:require [cljs.core.match :refer-macros [match]]
+            [cljs.core.async :refer [put! sub tap]]
+            [matthiasn.systems-toolbox.component :as comp]
+            [matthiasn.systems-toolbox.reagent :as r]
+            [matthiasn.systems-toolbox.log :as l]
+            [matthiasn.systems-toolbox.sente :as ws]))
+
+(defn make-comp
+  [app put-fn cfg]
+  (let [{:keys [cmp-id mk-state-fn handler-fn state-pub-handler-fn opts]} cfg
+        cmp (comp/make-component mk-state-fn handler-fn state-pub-handler-fn opts)]
+    (put-fn [:log/switchboard-init cmp-id])
+    (swap! app assoc-in [:components cmp-id] cmp)))
+
+(defn subscribe-component
+  "Subscribe component to a specified publisher."
+  [app put-fn params]
+  (let [pub-comp ((:pub-comp params) (:components @app))
+        sub-comp ((:sub-comp params) (:components @app))]
+    (sub (:state-pub pub-comp) :app-state (:sliding-in-chan sub-comp))
+    (put-fn [:log/switchboard-sub (str (:pub-comp params) "->" (:sub-comp params))])
+    (swap! app update-in [:subs] conj params)))
+
+(defn tap-comp
+  "Tap component in channel to a specified mult."
+  [app put-fn params]
+  (let [mult-comp ((:mult-comp params) (:components @app))
+        tap-comp  ((:tap-comp params)  (:components @app))]
+    (tap (:out-mult mult-comp) (:in-chan tap-comp))
+    (put-fn [:log/switchboard-tap (str (:mult-comp params) "->" (:tap-comp params))])
+    (swap! app update-in [:taps] conj params)))
+
+(defn make-ws-comp
+  "Initializes Sente / WS component and makes is accessible under [:components :ws]
+  inside the switchboard state atom."
+  [app put-fn]
+  (let [ws (ws/component)]
+    (swap! app assoc-in [:components :ws] ws)
+    (put-fn [:log/switchboard-init :ws])
+    ws))
+
+(defn make-reagent-comp
+  "Creates a Reagent component."
+  [app put-fn params]
+  (let [{:keys [cmp-id view-fn dom-id init-state]} params
+        cmp (r/component view-fn dom-id init-state)]
+    (put-fn [:log/switchboard-init-reagent cmp-id])
+    (swap! app assoc-in [:components cmp-id] cmp)))
+
+(defn make-log-comp
+  "Creates a log component."
+  [app put-fn]
+  (let [log-comp (l/component)]
+    (swap! app assoc-in [:components :log] log-comp)
+    (put-fn [:log/switchboard-init :log])
+    log-comp))
+
+(defn self-register
+  ""
+  [app put-fn self]
+  (swap! app assoc-in [:components :switchboard] self))
+
+(defn make-state
+  "Return clean initial component state atom."
+  [put-fn]
+  (let [app (atom {:components {}
+                   :subs #{}
+                   :taps #{}})]
+    app))
+
+(defn in-handler
+  "Handle incoming messages: process / add to application state."
+  [app put-fn msg]
+  (match msg
+         [:cmd/self-register self] (self-register app put-fn self)
+         [:cmd/make-comp      cmp] (make-comp app put-fn cmp)
+         [:cmd/make-ws-comp      ] (make-ws-comp app put-fn)
+         [:cmd/make-log-comp     ] (make-log-comp app put-fn)
+         [:cmd/make-r-comp params] (make-reagent-comp app put-fn params)
+         [:cmd/sub-comp    params] (subscribe-component app put-fn params)
+         [:cmd/tap-comp    params] (tap-comp app put-fn params)
+         :else (prn "unknown msg in switchboard-in-loop" msg)))
+
+(defn component
+  "Creates a switchboard component that wires individual components together into
+  a communicating system."
+  []
+  (prn "Switchboard starting.")
+  (let [switchboard (comp/make-component make-state in-handler nil)
+        sw-in-chan (:in-chan switchboard)]
+    (put! sw-in-chan [:cmd/self-register switchboard])
+    (put! sw-in-chan [:cmd/make-log-comp])
+    (put! sw-in-chan [:cmd/tap-comp {:mult-comp :switchboard :tap-comp :log}])
+    switchboard))
