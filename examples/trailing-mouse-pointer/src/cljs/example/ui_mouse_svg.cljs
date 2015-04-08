4766bbac349867262a412cabcb77f74dad4c1c01
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-04-08 16:56:45 +0200

    refactoring

--- examples/trailing-mouse-pointer/src/cljs/example/ui_mouse_svg.cljs
+++ examples/trailing-mouse-pointer/src/cljs/example/ui_mouse_svg.cljs
@@ -21,41 +21,64 @@
       (put-fn [:cmd/mouse-pos pos])
       (.stopPropagation ev))))
 
+(defn histogram-view
+  "Renders SVG with an area in which mouse moves are detected. They are then sent to the server and the round-trip
+  time is measured."
+  [rtt-times]
+  (let [freq (frequencies rtt-times)]
+    [:g
+     (for [[x y] freq] ^{:key (str x y)} [:rect {:x x :y (- 370 y) :fill "steelblue" :width 1 :height y}])
+     (for [x (range 50 1000 50)]
+       ^{:key (str "lines-x" x)} [:rect {:x x :y 372 :fill "black" :width 1 :height (if (= (mod x 100) 0) 10 5)}])
+     [:rect {:x 0 :y 371 :fill "black" :width 1000 :height 1}]
+     (for [x (range 100 1000 100)]
+       ^{:key (str "label-x" x)} [:text (merge text-default {:x x :y 391 :text-anchor :middle}) x])]))
+
+(defn trailing-circles
+  "Displays two transparent circles where one is drawn directly on the client and the other is drawn after a rountrip.
+  This makes it easier to experience any delays."
+  [state]
+  (let [pos (:pos state)
+        from-server (:from-server state)]
+    [:g
+     [:circle (merge circle-defaults {:cx (:x pos) :cy (:y pos)})]
+     [:circle (merge circle-defaults {:cx (:x from-server) :cy (:y from-server) :fill "rgba(0,0,255,0.1)"})]]))
+
+(defn text-view
+  "Renders SVG with an area in which mouse moves are detected. They are then sent to the server and the round-trip
+  time is measured."
+  [state pos mean mn mx latency]
+  [:g
+   [:text (merge text-bold {:y 12 :x 10}) "Mouse Moves Processed:"]
+   [:text (merge text-default {:y 12 :x 215}) (:count state)]
+   [:text (merge text-bold {:y 12 :x 265}) "Current Position:"]
+   (when pos [:text (merge text-default {:y 12 :x 405}) (str "x: " (:x pos) " y: " (:y pos))])
+   [:text (merge text-bold {:y 12 :x 530}) "Latency (ms):"]
+   (when latency [:text (merge text-default {:y 12 :x 640}) (str (.toFixed mean 0) "/" mn "/" mx "/" latency)])
+   [:text (merge text-default {:y 12 :x 840}) "(mean/min/max/last)"]])
+
 (defn mouse-view
   "Renders SVG with an area in which mouse moves are detected. They are then sent to the server and the round-trip
   time is measured."
   [app put-fn]
   (let [state @app
         pos (:pos state)
-        from-server (:from-server state)
         chart-w 1000 chart-h 400
         latency (:latency (:from-server state))
         rtt-times (:rtt-times state)
-        freq (frequencies rtt-times)
-        max (apply max rtt-times)
-        min (apply min rtt-times)
+        mx (apply max rtt-times)
+        mn (apply min rtt-times)
         mean (/ (apply + rtt-times) (count rtt-times))]
-    [:div
-     [:svg {:width chart-w :height chart-h :style {:background-color :white}
-            :on-mouse-move (mouse-move-ev-handler app put-fn (r/current-component))}
-      [:g
-       [:text (merge text-bold {:y 12 :x 10}) "Mouse Moves Processed:"]
-       [:text (merge text-default {:y 12 :x 215 }) (:count state)]
-       [:text (merge text-bold {:y 12 :x 265}) "Current Position:"]
-       (when pos [:text (merge text-default {:y 12 :x 405 }) (str "x: " (:x pos) " y: " (:y pos))])
-       [:text (merge text-bold {:y 12 :x 530}) "Latency (ms):"]
-       (when latency [:text (merge text-default {:y 12 :x 640 }) (str (.toFixed mean 0) "/" min "/" max "/" latency)])
-       [:text (merge text-default {:y 12 :x 740 }) "(mean/min/max/last)"]
-       [:circle (merge circle-defaults {:cx (:x pos) :cy (:y pos)})]
-       [:circle (merge circle-defaults {:cx (:x from-server) :cy (:y from-server) :fill "rgba(0,0,255,0.1)"})]
-       (for [[x y] freq] ^{:key (str x y)} [:rect {:x x :y (- 370 y) :fill "steelblue" :width 1 :height y}])
-       (for [x (range 50 1000 50)]
-         ^{:key (str "lines-x" x)} [:rect {:x x :y 372 :fill "black" :width 1 :height (if (= (mod x 100) 0) 10 5)}])
-       [:rect {:x 0 :y 371 :fill "black" :width 1000 :height 1}]
-       (for [x (range 100 1000 100)]
-         ^{:key (str "label-x" x)} [:text (merge text-default {:x x :y 391 :text-anchor :middle}) x])]]]))
+    [:svg {:width chart-w
+           :height chart-h
+           :style {:background-color :white}
+           :on-mouse-move (mouse-move-ev-handler app put-fn (r/current-component))}
+     [text-view state pos mean mn mx latency]
+     [trailing-circles state]
+     [histogram-view rtt-times]]))
 
 (defn mouse-pos-from-server!
+  "Handler function for mouse position messages received from server."
   [app pos]
   (let [latency (- (now) (:timestamp pos))
         with-ts (assoc pos :latency latency)]
