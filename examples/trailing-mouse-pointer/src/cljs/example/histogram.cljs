3019780566db8dbff19958669932adadac5b52ee
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-04-22 01:21:24 +0100

    histogram

---
+++ examples/trailing-mouse-pointer/src/cljs/example/histogram.cljs
@@ -0,0 +1,104 @@
+(ns example.histogram)
+
+(def text-default {:stroke "none" :fill "black" :style {:font-size 12}})
+(def text-bold (merge text-default {:style {:font-weight :bold :font-size 12}}))
+(def x-axis-label (merge text-default {:text-anchor :middle}))
+
+(def path-defaults {:fill :black :stroke :black :stroke-width 1})
+
+(defn interquartile-range
+  "Determines the interquartile range of values in a collection of numbers."
+  [sample]
+  (let [sorted (sort sample)
+        n (count sorted)
+        q1 (nth sorted (Math/floor (/ n 4)))
+        q3 (nth sorted (Math/floor (* (/ n 4) 3)))
+        iqr (- q3 q1)]
+    iqr))
+
+(defn percentile-range
+  "Returns only the values within the given percentile range."
+  [sample percentile]
+  (let [sorted (sort sample)
+        n (count sorted)
+        keep-n (Math/ceil (* n (/ percentile 100)))]
+    (take keep-n sorted)))
+
+(defn freedman-diaconis-rule
+  "Implements approximation of Freedman–Diaconis rule for determing bin size in histograms: bin size = 2 IQR(x) n^-1/3
+   where IQR(x) is the interquartile range of the data and n is the number of observations in the sample x.
+   Argument coll is expected to be a collection of numbers."
+  [sample]
+  (let [n (count sample)]
+    (when (pos? n)
+      (* 2 (interquartile-range sample) (Math/pow n (/ -1 3))))))
+
+(defn round-up
+  [n increment]
+  (* (Math/ceil (/ n increment)) increment))
+
+(defn round-down
+  [n increment]
+  (* (Math/floor (/ n increment)) increment))
+
+(defn histogram-y-axis
+  "Draws y-axis of a chart."
+  [x y h mx]
+  (let [increment (cond (> mx 250) 100 (> mx 100) 50 (> mx 50) 20 (> mx 25) 10 :else 5)
+        rng (range 0 (inc (round-up mx increment)) increment)
+        scale (/ h (dec (count rng)))]
+    [:g
+     [:path (merge path-defaults {:d (str "M" x " " y "l 0 " (* h -1) " z")})]
+     (for [n rng]
+       ^{:key (str "yt" n)}
+       [:path (merge path-defaults {:d (str "M" x " " (- y (* (/ n increment) scale)) "l -" 6 " 0")})])
+     (for [n rng]
+       ^{:key (str "yl" n)}
+       [:text (merge text-default {:x (- x 10) :y (- y (* (/ n increment) scale) -4) :text-anchor :end}) n])]))
+
+(defn histogram-x-axis
+  "Draws x-axis for histrogram."
+  [x y mn mx w scale]
+  (let [mx-mn (- mx mn)
+        increment (cond (> mx-mn 250) 50 (> mx-mn 90) 20 :else 10)
+        rng (range mn (inc mx) increment)]
+    [:g
+     [:path (merge path-defaults {:d (str "M" x " " y "l" w " 0 z")})]
+     (for [n rng] ^{:key (str "xt" n)}
+                  [:path (merge path-defaults {:d (str "M" (+ x (* (- n mn) scale)) " " y "l 0 " 6)})])
+     (for [n rng] ^{:key (str "xl" n)} [:text (merge x-axis-label {:x (+ x (* (- n mn) scale)) :y (+ y 20)}) n])]))
+
+(defn histogram-view
+  "Renders a histogram for roundtrip times."
+  [rtt-times x y w h x-label]
+    (let [mx (apply max rtt-times)
+          mn (apply min rtt-times)
+          rng (- mx mn)
+          mx2 (round-up (or mx 100) 10)
+          mn2 (round-down (or mn 0) 10)
+          rng2 (- mx2 mn2)
+          x-scale (/ w rng2)
+          bin-size (freedman-diaconis-rule rtt-times)
+          binned-freq (frequencies (map (fn [n] (Math/floor (/ (- n mn) bin-size))) rtt-times))
+          binned-freq-mx (apply max (map (fn [[_ f]] f) binned-freq))
+          bins (inc (apply max (map (fn [[v _]] v) binned-freq)))
+          bar-width (/ (* rng x-scale) bins)
+          y-scale (/ (- h 20) binned-freq-mx)]
+      [:g
+       ;[:rect {:x x :y (- y h) :stroke :red :width w :height h :fill "rgba(255,0,0,0.1)"}]
+       ;[:rect {:x (- x 50) :y (- y h 10) :stroke :green :width (+ w 70) :height (+ 60 h) :fill "rgba(0,255,0,0.1)"}]
+       (if (> bins 4)
+         (for [[v f] binned-freq]
+           ^{:key (str "bf" v f)} [:rect {:x      (+ x (* (- mn mn2) x-scale) (* v bar-width))
+                                          :y      (- y (* f y-scale))
+                                          :fill   "steelblue" :stroke "black"
+                                          :width  bar-width
+                                          :height (* f y-scale)}])
+
+         [:text {:x (+ x (/ w 2)) :y (- y 50) :stroke "none" :fill "#DDD" :text-anchor :middle
+                 :style {:font-weight :bold :font-size 24}} "insufficient data"])
+       [histogram-x-axis x (+ y 7) mn2 mx2 w x-scale]
+       [:text (merge x-axis-label text-bold {:x (+ x (/ w 2)) :y (+ y 48) :text-anchor :middle}) x-label]
+       [:text (let [x-coord (- x 45) y-coord (- y (/ h 3)) rotate (str "rotate(270 " x-coord " " y-coord ")")]
+                (merge x-axis-label text-bold {:x x-coord :y y-coord :transform rotate})) "Frequencies"]
+       [histogram-y-axis (- x 7) y h (or binned-freq-mx 10)]]))
--- examples/trailing-mouse-pointer/src/cljs/example/ui_mouse_svg.cljs
+++ examples/trailing-mouse-pointer/src/cljs/example/ui_mouse_svg.cljs
@@ -1,5 +1,6 @@
 (ns example.ui-mouse-svg
   (:require [reagent.core :as r :refer [atom]]
+            [example.histogram :as hist]
             [matthiasn.systems-toolbox.component :as comp]
             [matthiasn.systems-toolbox.helpers :refer [by-id]]
             [cljs.core.match :refer-macros [match]]))
@@ -23,124 +24,6 @@
       (swap! app assoc :pos pos)
       (put-fn [:cmd/mouse-pos pos])
       (.stopPropagation ev))))
-
-(def path-defaults {:fill :black :stroke :black :stroke-width 1})
-
-(defn interquartile-range
-  "Determines the interquartile range of values in a collection of numbers."
-  [sample]
-  (let [sorted (sort sample)
-        n (count sorted)
-        q1 (nth sorted (Math/floor (/ n 4)))
-        q3 (nth sorted (Math/floor (* (/ n 4) 3)))
-        iqr (- q3 q1)]
-    iqr))
-
-(defn freedman-diaconis-rule
-  "Implements approximation of Freedman–Diaconis rule for determing bin size in histograms: bin size = 2 IQR(x) n^-1/3
-   where IQR(x) is the interquartile range of the data and n is the number of observations in the sample x.
-   Argument coll is expected to be a collection of numbers."
-  [sample]
-  (let [n (count sample)]
-    (when (pos? n)
-      (* 2 (interquartile-range sample) (Math/pow n (/ -1 3))))))
-
-(defn tick-length
-  "Determines length of tick for the chart axes."
-  [n]
-  (cond
-    (zero? (mod n 100)) 9
-    (zero? (mod n 50)) 6
-    :else 3))
-
-(defn x-axis
-  "Draws x-axis of a chart."
-  [x y l scale]
-  [:g
-   [:path (merge path-defaults {:d (str "M" x " " y "l" (* l scale) " 0 l 0 -4 l 10 4 l -10 4 l 0 -4 z")})]
-   (for [n (range 0 l 10)]
-     ^{:key (str "xt" n)} [:path (merge path-defaults {:d (str "M" (+ x (* n scale)) " " y "l 0 " (tick-length n))})])
-   (for [n (range 0 l 50)]
-     ^{:key (str "xl" n)} [:text (merge x-axis-label {:x (+ x (* n scale)) :y (+ y 20)}) n])])
-
-(defn x-axis2
-  "Draws x-axis of a chart."
-  [x y l scale]
-  [:g
-   [:path (merge path-defaults {:d (str "M" x " " y "l" (* l scale) " 0 l 0 -4 l 10 4 l -10 4 l 0 -4 z")})]
-   (for [n (range 0 l 10)]
-     ^{:key (str "xt" n)} [:path (merge path-defaults {:d (str "M" (+ x (* n scale)) " " y "l 0 " (tick-length n))})])
-   (for [n (range 0 l 50)]
-     ^{:key (str "xl" n)} [:text (merge x-axis-label {:x (+ x (* n scale)) :y (+ y 20)}) n])])
-
-(defn y-axis
-  "Draws y-axis of a chart."
-  [x y l scale]
-  [:g
-   [:path (merge path-defaults {:d (str "M" x " " y "l 0 " (* l scale -1) " l -4 0 l 4 -10 l 4 10 l -4 0 z")})]
-   (for [n (range 0 l 10)]
-     ^{:key (str "yt" n)} [:path (merge path-defaults
-                                        {:d (str "M" x " " (- y (* n scale)) "l -" (tick-length n) " 0")})])
-   (for [n (range 0 l 50)]
-     ^{:key (str "yl" n)} [:text (merge text-default {:x (- x 10) :y (- y (* n scale) -4) :text-anchor :end}) n])])
-
-(defn histogram-view
-  "Renders a histogram for roundtrip times."
-  [rtt-times x y w h]
-  (let [freq (frequencies rtt-times)
-        max-freq (apply max (map (fn [[_ f]] f) freq))
-        scale 2
-        mx (apply max rtt-times)
-        x-axis-l (max (+ (* (Math/ceil (/ mx 50)) 50) 20) 130)
-        y-axis-l (min (max (+ (* (Math/ceil (/ max-freq 10)) 10) 20) 70) 110)]
-    [:g
-     (for [[v f] freq]
-       ^{:key (str "b" v f)} [:rect {:x (+ x (* v scale)) :y (- y (* f scale))
-                                     :fill "steelblue" :width 1.3 :height (* f scale)}])
-     [x-axis x y x-axis-l scale]
-     [:text (merge x-axis-label text-bold {:x (+ x x-axis-l -10) :y (+ y 40)}) "Roundtrip t/ms"]
-     [:text (let [x-coord (- x 45) y-coord (- y y-axis-l 10) rotate (str "rotate(270 " x-coord " " y-coord ")")]
-              (merge x-axis-label text-bold {:x x-coord :y y-coord :transform rotate})) "Frequencies"]
-     [y-axis x y y-axis-l scale]]))
-
-(defn y-axis2
-  "Draws y-axis of a chart."
-  [x y l scale]
-  [:g
-   [:path (merge path-defaults {:d (str "M" x " " y "l 0 " (* l -1) " l -4 0 l 4 -10 l 4 10 l -4 0 z")})]
-   (for [n (range 0 l 10)]
-     ^{:key (str "yt" n)} [:path (merge path-defaults
-                                        {:d (str "M" x " " (- y (* n scale)) "l -" (tick-length n) " 0")})])
-   (for [n (range 0 l 50)]
-     ^{:key (str "yl" n)} [:text (merge text-default {:x (- x 10) :y (- y (* n scale) -4) :text-anchor :end}) n])])
-
-(defn histogram-view2
-  "Renders a histogram for roundtrip times."
-  [rtt-times x y w h]
-    (let [mx (apply max rtt-times)
-          mn (apply min rtt-times)
-          bin-size (freedman-diaconis-rule rtt-times)
-          bins (Math/round (/ (- mx mn) bin-size))
-          binned-freq (frequencies (map (fn [n] (Math/floor (/ (- n mn) bin-size))) rtt-times))
-          binned-freq-mx (apply max (map (fn [[_ f]] f) binned-freq))
-          x-axis-l (max (+ (* (Math/ceil (/ mx 50)) 50) 20) 130)
-          ;y-axis-l (min (max (+ (* (Math/ceil (/ max-freq 10)) 10) 20) 70) 110)
-          y-axis-l 100
-          ;y-axis-l (+ (* (Math/ceil (/ binned-freq-mx 10)) 10) 20)
-          y-scale (/ (- h 20) binned-freq-mx)]
-      [:g
-       [:rect {:x x :y (- y h) :stroke :red :width w :height h :fill "rgba(0,0,255,0.1)"}]
-       [:rect {:x (- x 45) :y (- y h) :stroke :red :width (+ w 45) :height (+ 40 h) :fill "rgba(0,255,0,0.1)"}]
-       (when (pos? (count rtt-times))
-         (for [[v f] binned-freq]
-         ^{:key (str "bf" v f)} [:rect {:x (+ x (* mn 2) (* v 2 bin-size)) :y (- y (* f y-scale)) :fill "steelblue"
-                                        :stroke "black" :width (* bin-size 2) :height (* f y-scale)}]))
-       [x-axis x y x-axis-l 2]
-       [:text (merge x-axis-label text-bold {:x (+ x x-axis-l -10) :y (+ y 38)}) "Roundtrip t/ms"]
-       [:text (let [x-coord (- x 35) y-coord (- y (/ h 3)) rotate (str "rotate(270 " x-coord " " y-coord ")")]
-                (merge x-axis-label text-bold {:x x-coord :y y-coord :transform rotate})) "Frequencies"]
-       [y-axis2 x y h y-scale]
-       ]))
 
 (defn trailing-circles
   "Displays two transparent circles where one is drawn directly on the client and the other is drawn after a roundtrip.
@@ -182,8 +65,9 @@
            :on-mouse-move (mouse-move-ev-handler app put-fn (r/current-component))}
      [text-view state pos (.toFixed mean 0) mn mx latency]
      [trailing-circles state]
-     [histogram-view rtt-times 90 280 300 200]
-     [histogram-view2 rtt-times 500 280 300 150]]))
+     [hist/histogram-view rtt-times 100 280 300 160 "Roundtrip t/ms"]
+     [hist/histogram-view (hist/percentile-range rtt-times 99) 500 280 300 160
+      "Roundtrip t/ms (within 99th percentile)"]]))
 
 (defn mouse-pos-from-server!
   "Handler function for mouse position messages received from server."
