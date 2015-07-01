7cba2b6947d101ac942593c0d2b11e475ad43947
batwicket <james_fremen@yahoo.com>
2015-07-01 17:14:36 -0400

    integration of figwheel into build.

--- examples/trailing-mouse-pointer/project.clj
+++ examples/trailing-mouse-pointer/project.clj
@@ -16,6 +16,7 @@
                  [reagent "0.5.0"]
                  [incanter "1.5.6"]
                  [clj-time "0.9.0"]
+                 [lein-figwheel "0.3.5"]
                  [org.clojure/clojurescript "0.0-3308"]]
 
   :source-paths ["src/clj/"]
@@ -25,11 +26,15 @@
   :main example.core
 
   :plugins [[lein-cljsbuild "1.0.6"]
+            [lein-figwheel "0.3.5"]
             [codox "0.8.10"]]
 
   :cljsbuild {:builds [{:id "release"
-                        :source-paths ["src/cljs"]
-                        :compiler {:optimizations :advanced
+                        :source-paths ["src/cljs" "env/dev/cljs"]
+                        :figwheel true
+                        :compiler {:main "reagent-proj.dev"
+                                   :asset-path "js/build"
+                                   :optimizations :none
                                    ;:optimizations :simple
                                    ;:source-map "resources/public/js/build/example.js.map"
                                    :output-dir "resources/public/js/build/"
