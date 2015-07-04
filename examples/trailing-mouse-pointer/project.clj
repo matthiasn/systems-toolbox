b6fa59fa70e7d8da6f9b429c8c4efe6902b7cc53
batwicket <james_fremen@yahoo.com>
2015-07-04 12:05:31 -0400

    integrate figwheel into build

--- examples/trailing-mouse-pointer/project.clj
+++ examples/trailing-mouse-pointer/project.clj
@@ -15,6 +15,7 @@
                  [reagent "0.5.0"]
                  [incanter "1.5.6"]
                  [clj-time "0.9.0"]
+                 [lein-figwheel "0.3.5"]
                  [org.clojure/clojurescript "0.0-3308"]]
 
   :source-paths ["src/clj/"]
@@ -24,12 +25,21 @@
   :main example.core
 
   :plugins [[lein-cljsbuild "1.0.6"]
+            [lein-figwheel "0.3.5"]
             [codox "0.8.10"]]
 
-  :cljsbuild {:builds [{:id "release"
-                        :source-paths ["src/cljs"]
-                        :compiler {:optimizations :advanced
-                                   ;:optimizations :simple
-                                   ;:source-map "resources/public/js/build/example.js.map"
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src/cljs" "env/dev/cljs"]
+                        :figwheel true
+                        :compiler {:main "example.dev"
+                                   :asset-path "js/build"
+                                   :optimizations :none
                                    :output-dir "resources/public/js/build/"
-                                   :output-to "resources/public/js/build/example.js"}}]})
+                                   :output-to "resources/public/js/build/example.js"}}
+                       {:id "release"
+                        :source-paths ["src/cljs"]
+                        :figwheel true
+                        :compiler {:main "example.core"
+                                   :asset-path "js/build"
+                                   :output-to "resources/public/js/build/example.js"
+                                   :optimizations :advanced}}]})
--- examples/trailing-mouse-pointer/src/cljs/example/core.cljs
+++ examples/trailing-mouse-pointer/src/cljs/example/core.cljs
@@ -12,23 +12,27 @@
 
 (def switchboard (sb/component :client/switchboard))
 
-(sb/send-mult-cmd
-  switchboard   ;; Below, we're interacting with the switchboard component we just created above.
-  [;; First of all, we instantiate and wire a couple fo different components.
-   [:cmd/wire-comp (sente/component :client/ws-cmp)]        ; WebSocket communication component
-   [:cmd/wire-comp (hist/component :client/histogram-cmp)]  ; UI component for histograms
-   [:cmd/wire-comp (mouse/component :client/mouse-cmp)]     ; UI component for capturing mouse moves
-   [:cmd/wire-comp (store/component :client/store-cmp)]     ; Data store component
-   [:cmd/wire-comp (jvmstats/component :client/jvmstats-cmp "jvm-stats-frame")] ;  UI component: JVM stats
-   [:cmd/wire-comp (obs/component :client/observer-cmp conf/observer-cfg-map)]  ; UI component for observing system
+(defn init
+  []
+  (sb/send-mult-cmd
+    switchboard   ;; Below, we're interacting with the switchboard component we just created above.
+    [;; First of all, we instantiate and wire a couple fo different components.
+     [:cmd/wire-comp (sente/component :client/ws-cmp)]        ; WebSocket communication component
+     [:cmd/wire-comp (hist/component :client/histogram-cmp)]  ; UI component for histograms
+     [:cmd/wire-comp (mouse/component :client/mouse-cmp)]     ; UI component for capturing mouse moves
+     [:cmd/wire-comp (store/component :client/store-cmp)]     ; Data store component
+     [:cmd/wire-comp (jvmstats/component :client/jvmstats-cmp "jvm-stats-frame")] ;  UI component: JVM stats
+     [:cmd/wire-comp (obs/component :client/observer-cmp conf/observer-cfg-map)]  ; UI component for observing system
 
-   ;; Then, messages of a given type are wired from one component to another.
-   ;[:cmd/route {:from :client/mouse-cmp :to :client/ws-cmp :only :cmd/mouse-pos}]
-   [:cmd/route-all {:from :client/mouse-cmp :to :client/ws-cmp}]
-   [:cmd/route {:from :client/ws-cmp :to :client/store-cmp}]
-   [:cmd/route {:from :client/ws-cmp :to :client/jvmstats-cmp}]
-   [:cmd/observe-state {:from :client/store-cmp :to :client/histogram-cmp}]
-   [:cmd/observe-state {:from :client/store-cmp :to :client/mouse-cmp}]
+     ;; Then, messages of a given type are wired from one component to another.
+     ;[:cmd/route {:from :client/mouse-cmp :to :client/ws-cmp :only :cmd/mouse-pos}]
+     [:cmd/route-all {:from :client/mouse-cmp :to :client/ws-cmp}]
+     [:cmd/route {:from :client/ws-cmp :to :client/store-cmp}]
+     [:cmd/route {:from :client/ws-cmp :to :client/jvmstats-cmp}]
+     [:cmd/observe-state {:from :client/store-cmp :to :client/histogram-cmp}]
+     [:cmd/observe-state {:from :client/store-cmp :to :client/mouse-cmp}]
 
-   ;; Finally, wire firehose with all messages into the observer component.
-   [:cmd/attach-to-firehose :client/observer-cmp]])
+     ;; Finally, wire firehose with all messages into the observer component.
+     [:cmd/attach-to-firehose :client/observer-cmp]])
+  )
+(init)
