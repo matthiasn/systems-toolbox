80be8118627de132ceea94c145a2dbaccdd5ace6
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2015-10-26 15:23:24 +0100

    example fixed

--- examples/trailing-mouse-pointer/project.clj
+++ examples/trailing-mouse-pointer/project.clj
@@ -5,6 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [ch.qos.logback/logback-classic "1.1.3"]
