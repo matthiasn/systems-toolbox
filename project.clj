6c58c2d4be50466a26dd3d05ae44a75895dd709e
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-01-09 00:57:22 +0100

    v0.5.2: latest version of ClojureScript

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.reader "1.0.0-alpha1"]
-                 [org.clojure/clojurescript "1.7.170"]
+                 [org.clojure/clojurescript "1.7.228"]
                  [org.ow2.asm/asm-all "5.0.4"]
                  [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/tools.logging "0.3.1"]
