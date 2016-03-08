34d7558596fe8d9391d4830f56b5fb96267e589a
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-03-08 03:54:52 +0100

    latest dependencies

--- project.clj
+++ project.clj
@@ -10,24 +10,24 @@
                  [org.clojure/tools.reader "1.0.0-alpha1"]
                  [org.clojure/clojurescript "1.7.228"]
                  [org.clojure/core.match "0.3.0-alpha4" :exclusions [org.clojure/tools.analyzer.jvm]]
-                 [org.ow2.asm/asm-all "5.0.4"]
+                 [org.ow2.asm/asm-all "5.1"]
                  [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
                  [org.clojure/tools.logging "0.3.1"]
-                 [io.aviso/pretty "0.1.23"]
+                 [io.aviso/pretty "0.1.24"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]]
 
   :plugins [[lein-codox "0.9.4"]
             [test2junit "1.2.1"]
             [lein-doo "0.1.6"]
-            [lein-cljsbuild "1.1.2"]]
+            [lein-cljsbuild "1.1.3"]]
 
   :test2junit-output-dir ~(or (System/getenv "CIRCLE_TEST_REPORTS") "target/test2junit")
 
   :profiles {:dev {:dependencies [[org.clojure/tools.logging "0.3.1"]
-                                  [ch.qos.logback/logback-classic "1.1.5" :exclusions [org.slf4j/slf4j-api]]
-                                  [org.slf4j/jul-to-slf4j "1.7.16"]
-                                  [org.slf4j/jcl-over-slf4j "1.7.16"]
-                                  [org.slf4j/log4j-over-slf4j "1.7.16"]]}}
+                                  [ch.qos.logback/logback-classic "1.1.6" :exclusions [org.slf4j/slf4j-api]]
+                                  [org.slf4j/jul-to-slf4j "1.7.18"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.18"]
+                                  [org.slf4j/log4j-over-slf4j "1.7.18"]]}}
 
   :cljsbuild {:builds [{:id           "cljs-test"
                         :source-paths ["src" "test"]
