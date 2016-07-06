ed323628bc9b9c709e768d91e767959d8b0e021c
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-07-06 02:56:59 +0200

    deps

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.ow2.asm/asm-all "5.1"]
                  [org.clojure/core.async "0.2.385"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [io.aviso/pretty "0.1.26"]
+                 [io.aviso/pretty "0.1.27"]
                  [com.lucasbradstreet/cljs-uuid-utils "1.0.2"]]
 
   :plugins [[lein-codox "0.9.5"]
@@ -27,7 +27,7 @@
   :test-paths ["test"]
   ;:test-paths ["test" "perf"]
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha8"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0-alpha9"]
                                   [org.clojure/clojurescript "1.9.93"
                                    :exclusions [org.clojure/tools.reader]]
                                   [org.clojure/tools.logging "0.3.1"]
