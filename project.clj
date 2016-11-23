02e70e81e8a11ddaddef7b123f0932bb729d3b47
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-11-23 22:55:10 +0100

    v0.6.1: moving away from alpha status

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox "0.6.1-alpha11"
+(defproject matthiasn/systems-toolbox "0.6.1"
   :description "Toolbox for building Systems in Clojure"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
