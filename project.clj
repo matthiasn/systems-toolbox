1ab258b82f4069143c8b0719e3b707bab9d879b4
Kamil Leszczuk <kamituel@gmail.com>
2017-05-30 11:59:02 +0200

    Make switchboard's spec validation be configurable.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matthiasn/systems-toolbox "0.6.8"
+(defproject matthiasn/systems-toolbox "0.6.9"
   :description "Toolbox for building Systems in Clojure"
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
@@ -26,7 +26,7 @@
 
   :test-refresh {:notify-on-success false
                  :changes-only      false}
-  
+
   :test-paths ["test"]
   ;:test-paths ["dev-resources" "test" "perf"]
 
