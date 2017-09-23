de66f17c136c5ef0da1f3900aba40d172b8da043
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2017-09-23 14:46:55 +0200

    updated deps in example

--- examples/redux-counter01/project.clj
+++ examples/redux-counter01/project.clj
@@ -3,16 +3,16 @@
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojurescript "1.9.562"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+                 [org.clojure/clojurescript "1.9.908"]
                  [hiccup "1.0.5"]
-                 [re-frame "0.9.3"]
+                 [re-frame "0.10.1"]
                  [clj-pid "0.1.2"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [org.clojure/tools.logging "0.3.1"]
+                 [org.clojure/tools.logging "0.4.0"]
                  [matthiasn/systemd-watchdog "0.1.3"]
-                 [matthiasn/systems-toolbox "0.6.9"]
-                 [matthiasn/systems-toolbox-sente "0.6.15"]]
+                 [matthiasn/systems-toolbox "0.6.17"]
+                 [matthiasn/systems-toolbox-sente "0.6.17"]]
 
   :source-paths ["src/clj/"]
 
@@ -20,8 +20,8 @@
 
   :main example.core
 
-  :plugins [[lein-cljsbuild "1.1.6"]
-            [lein-figwheel "0.5.10"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.13"]]
 
   :figwheel {:server-port 3452
              :css-dirs    ["resources/public/css"]}
